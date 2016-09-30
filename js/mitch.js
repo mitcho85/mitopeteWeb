4c525bf0ca49e5edad98db1f96a1cbbdbe28c6f9
mitcho85 <mitopete@ucsd.edu>
2016-09-30 01:30:36 -0700

    Initial code push

---
+++ js/mitch.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	$(document).foundation();
+	// SLick Slider Code
+	var $windowWidth=$(window).width();
+	$( window ).resize(function() {
+		var $windowWidth=$(window).width();
+		if(($windowWidth > 1) && ($windowWidth < 640)){
+		$('.slick-multislider').unslick();	
+			$('.slick-multislider').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			dots: true,
+			});
+	    } else {
+		$('.slick-multislider').unslick();	   	
+			$('.slick-multislider').slick({
+				slidesToShow: 3,
+				slidesToScroll: 2,
+				autoplay: true,
+				autoplaySpeed: 2000,
+				dots: true,
+			});
+		}	
+	});
+	if(($windowWidth > 1) && ($windowWidth < 640)){
+      $('.slick-multislider').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          dots: true,
+      });
+    } else {
+        $('.slick-multislider').slick({
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          dots: true,
+        });
+    }
+});
